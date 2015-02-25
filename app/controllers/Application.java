d77d18f16b76e1a8825315d14b60e65d600ab8c5
Alberto Almagro <albertoalmagro@gmail.com>
2015-02-25 21:32:59 +0100

    Added route for login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,12 +4,16 @@ import models.Project;
 import models.Task;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
+import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render(Project.find.all(), Task.find.all()));
+    }
+    
+    public static Result login() {
+        return ok(login.render());
     }
 
 }
