71ca54aa05094814c7cce2137ffc57d22d2c1ad8
Alberto Almagro <albertoalmagro@gmail.com>
2015-02-27 21:25:02 +0100

    Added tests to login

--- test/models/LoginTest.java
+++ test/models/LoginTest.java
@@ -1,16 +1,36 @@
 package models;
 
-import java.util.List;
+import org.junit.Test;
 
-import org.junit.Before;
+import com.google.common.collect.ImmutableMap;
 
-import play.libs.Yaml;
-import play.test.WithApplication;
-import static play.test.Helpers.*;
+import play.mvc.Result;
+import play.test.Helpers;
 import base.AbstractTest;
 
-import com.avaje.ebean.Ebean;
+import static org.junit.Assert.assertEquals; 
+import static org.junit.Assert.assertNull; 
 
 public class LoginTest extends AbstractTest {
+	
+	@Test
+	public void authenticateSuccess() {
+		Result result = Helpers.callAction(controllers.routes.ref.Application.authenticate(),
+				Helpers.fakeRequest().withFormUrlEncodedBody(ImmutableMap.of(
+						"email", "bob@example.com",
+						"password", "secret")));
+		assertEquals(303, Helpers.status(result));
+		assertEquals("bob@example.com", Helpers.session(result).get("email"));
+	}
+	
+	@Test
+	public void authenticateFailure() {
+		Result result = Helpers.callAction(controllers.routes.ref.Application.authenticate(),
+				Helpers.fakeRequest().withFormUrlEncodedBody(ImmutableMap.of(
+						"email", "bob@example.com",
+						"password", "badpassword")));
+		assertEquals(400, Helpers.status(result));
+		assertNull(Helpers.session(result).get("email"));
+	}
 
 }
