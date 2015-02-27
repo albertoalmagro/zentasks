e898caf1a5c88c504b7ed18b344555988827b3c4
Alberto Almagro <albertoalmagro@gmail.com>
2015-02-27 20:54:11 +0100

    Fixed unit tests.

--- test/models/AbstractModelTest.java
+++ test/models/AbstractModelTest.java
@@ -1,17 +1,12 @@
 package models;
 
 import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.fakeGlobal;
 import static play.test.Helpers.inMemoryDatabase;
-
-import java.util.List;
 
 import org.junit.Before;
 
-import play.db.ebean.Model;
-import play.libs.Yaml;
 import play.test.WithApplication;
-
-import com.avaje.ebean.Ebean;
 
 /**
  * Abstract Class that set up a fake application with an in memory database.
@@ -23,17 +18,11 @@ public abstract class AbstractModelTest extends WithApplication {
 
 	@Before
 	public void setUp() {
-		loadTestData();
 		startFakeApplicationWithInMemoryDatabase();
 	}
 
 	private void startFakeApplicationWithInMemoryDatabase() {
-		fakeApplication(inMemoryDatabase());
+		fakeApplication(inMemoryDatabase(), fakeGlobal());
 	}
 	
-	@SuppressWarnings("rawtypes")
-	private void loadTestData() {
-		Ebean.save((List) Yaml.load("test-data.yml"));
-	}
-
 }
